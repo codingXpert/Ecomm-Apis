1380bec56a270032fe935678ad0c5cecd94f9661
Vivek <vivekraj78965.vr@gmail.com>
2024-01-13 02:21:35 +0530

    readme setup

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const express = require('express');
 const PORT = 8000;
 const app = express();
 const db = require('./config/mongoose');
+const YAML = require('yamljs');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDocs = YAML.load('./api.yaml');
 
 
 app.use(express.json());
 app.use('/api', require('./routes'))
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
 
 app.listen(PORT, (err) => {
     if(err){
