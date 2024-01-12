b2ca39c7b0875eb40d4bcc694e085dd195451cc5
Vivek <vivekraj78965.vr@gmail.com>
2024-01-12 16:52:03 +0530

    modification in update product route

--- controllers/productController.js
+++ controllers/productController.js
@@ -47,12 +47,18 @@ exports.getProducts = async (req, res) => {
 exports.updateProduct = async (req, res) => {
     const { productId } = req.params;
     try {
-        const updateProduct = await Product.findByIdAndUpdate(productId, req.body, { new: true });
+        if(productId) { 
+            const updateProduct = await Product.findByIdAndUpdate(productId, req.body, { new: true });
         if(!updateProduct) {
             return res.status(404).json({ status: 'failed', error: 'Product not found' });
         }
         res.status(200).json({ status: 'success', message: 'Product updated successfully', updateProduct });
+        } else {
+            res.status(400).json({ status: 'failed', message: 'Provide the productId' });
+        }
+        
     } catch (error) {
         res.status(500).json({ status: 'failed', error: error.message });
     }
 }
+
